2ecc229c36ae6e020f556fb93246478a174eacc8
sogaoh <sogaoh@ant-in-giant.work>
2019-04-07 21:27:47 +0900

    [WIP] aws-01 でもこれインターネットに出れない。。 4

--- aws-01/network.tf
+++ aws-01/network.tf
@@ -8,40 +8,6 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_internet_gateway" "gw" {
-  vpc_id = "${aws_vpc.main.id}"
-
-  tags {
-    Name = "internet-gw-myaws-01"
-  }
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = "${aws_vpc.main.id}"
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.gw.id}"
-  }
-
-  tags{
-    Name = "public-rt-myaws-01"
-  }
-}
-
-resource "aws_route_table" "private" {
-  vpc_id = "${aws_vpc.main.id}"
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = "${aws_nat_gateway.default.id}"
-  }
-
-  tags{
-    Name = "private-rt-myaws-01"
-  }
-}
-
 resource "aws_subnet" "public" {
   vpc_id = "${aws_vpc.main.id}"
   cidr_block = "10.1.1.0/24"
@@ -62,25 +28,3 @@ resource "aws_subnet" "private" {
   }
 }
 
-resource "aws_eip" "nat" {
-  vpc = true
-}
-
-resource "aws_nat_gateway" "default" {
-  allocation_id = "${aws_eip.nat.id}"
-  subnet_id = "${aws_subnet.public.id}"
-
-  tags{
-    Name = "nat-gw-myaws-01"
-  }
-}
-
-resource "aws_route_table_association" "public" {
-  subnet_id = "${aws_subnet.public.id}"
-  route_table_id = "${aws_route_table.public.id}"
-}
-
-resource "aws_route_table_association" "private" {
-  subnet_id = "${aws_subnet.private.id}"
-  route_table_id = "${aws_route_table.private.id}"
-}
